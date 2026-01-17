458555c6dba31a446b2537c59a249631f316ec0c
QSP <QSP@DESKTOP-K5OA249>
2026-01-17 08:41:06 +0530

    commited

---
+++ src/test/java/Pract/Demo1Test.java
@@ -0,0 +1,14 @@
+package Pract;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test(groups = "smoke")
+	public void appleTest()
+	{
+		System.out.println("appletest");
+	}
+	
+	
+}
---
+++ src/test/java/Pract/DemoTest.java
@@ -0,0 +1,12 @@
+package Pract;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test(groups = "regression")
+	public void mangoTest()
+	{
+		System.out.println("mangotest");
+	}
+}
